190ca98ac3052a2179c3e252ff5d021fd8a938a1
Suyeol Jeon <devxoul@gmail.com>
2015-09-24 17:00:14 +0900

    Initial Commit :)

---
+++ ModelKit/ModelKit.h
@@ -0,0 +1,19 @@
+//
+//  ModelKit.h
+//  ModelKit
+//
+//  Created by 전수열 on 9/24/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModelKit.
+FOUNDATION_EXPORT double ModelKitVersionNumber;
+
+//! Project version string for ModelKit.
+FOUNDATION_EXPORT const unsigned char ModelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelKit/PublicHeader.h>
+
+
